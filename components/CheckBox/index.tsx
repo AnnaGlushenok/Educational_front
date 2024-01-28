396ff15649d758d4e8ccf66c9ed0f64a60433035
annas <annasan14052003@gmail.com>
2024-01-28 18:27:52 +0300

    changed CheckBox to Items

--- components/CheckBox/index.tsx
+++ components/CheckBox/index.tsx
@@ -1,26 +1,38 @@
 import s from "./style.module.css";
 import {StringItem} from "@/interfaces/StringItem";
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent} from 'react';
+import {Questions} from "@/enum/Questions";
 
-export default function CheckBox({topic, onCheckboxChange}: {
+export default function Items({topic, type, onCheckboxChange, image}: {
     topic: StringItem,
-    onCheckboxChange: (isChecked: boolean, id: number) => void
+    type: Questions,
+    onCheckboxChange: (isChecked: boolean, id: number, name: string) => void,
+    image: boolean
 }) {
     const handleCheckboxChange = (event: ChangeEvent<HTMLInputElement>) => {
         const isChecked = event.target.checked;
-        onCheckboxChange(isChecked, topic.id);
+        onCheckboxChange(isChecked, topic.id, topic.name);
     };
 
-    return (
-        <label htmlFor={topic.id.toString()} className={s.item}>
+    return image ? (
+            <label htmlFor={topic.id.toString()} className={s.item}>
+                <input
+                    type={type}
+                    style={{display: 'none'}}
+                    id={topic.id.toString()}
+                    className={s.hidden_checkbox}
+                    onChange={handleCheckboxChange}/>
+                <div className={s.check_box}></div>
+                <p className={s.text}>{topic.name}</p>
+            </label>
+        ) :
+        <div key={topic.name} className={s.card__question}>
             <input
-                type="checkbox"
-                style={{display: 'none'}}
-                id={topic.id.toString()}
-                className={s.hidden_checkbox}
+                type={type}
+                id={topic.name}
+                name={type}
+                className={s.question__checkbox}
                 onChange={handleCheckboxChange}/>
-            <div className={s.check_box}></div>
-            <p className={s.text}>{topic.name}</p>
-        </label>
-    )
+            <label htmlFor={topic.name}>{topic.name}</label>
+        </div>
 }
